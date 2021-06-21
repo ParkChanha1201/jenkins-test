f18a5359a887ca06d1adec6bfd894ee0e392052e
박찬하 <chanhacraft@naver.com>
2021-06-21 10:26:02 +0900

    first commit

---
+++ src/main/java/com/chcraft/test/Testtest1Application.java
@@ -0,0 +1,13 @@
+package com.chcraft.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testtest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testtest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/chcraft/test/Testtest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chcraft.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testtest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
